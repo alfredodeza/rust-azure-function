f4ebc3d49abd2afdf7e9c35523c349ef19d1a5c7
Alfredo Deza <alfredo@deza.pe>
2023-02-14 08:56:46 -0500

    update to serialize the response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
-
+use std::env;
 
 //create a function that accepts a string as input and returns a sha256 hash of the string
 pub fn sha(text: &str) -> String {
@@ -12,21 +12,25 @@ pub fn sha(text: &str) -> String {
     format!("{:x}", result)
 }
 
-
 //create a struct that will be used to deserialize the JSON payload
 #[derive(Deserialize)]
 struct Text {
     text: String,
 }
 
+//create a struct to serialize the response using serde and hash as the key
+#[derive(serde::Serialize)]
+struct Hash {
+    hash: String,
+}
 
 #[post("/api/token")]
 async fn tokenize(text: web::Json<Text>) -> impl Responder {
     //use the hash function to create a hash of the text and then return the hash
     //as JSON
     let hash = sha(&text.text);
-    //use HttpResponse to return the hash as JSON
-    HttpResponse::Ok().body(hash.to_string())
+    // return the hash as JSON using the Hash struct defined above
+    HttpResponse::Ok().json(Hash { hash })
 }
 
 #[actix_web::main]
@@ -34,12 +38,11 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
-    use std::env;
     let port: u16 = match env::var(port_key) {
         Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
         Err(_) => 3000,
     };
-    // run the server on port 8001 and use the default logger as middleware
+
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
